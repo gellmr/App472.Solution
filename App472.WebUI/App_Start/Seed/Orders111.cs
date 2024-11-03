8ab0881829f84c69daf3fe1c73779e71733699f8
gellmr <gellmr@users.noreply.github.com>
2024-11-03 20:55:42 +0800

    Updated dev seed code for Orders111 and 112

--- App472.WebUI/App_Start/Seed/Orders111.cs
+++ App472.WebUI/App_Start/Seed/Orders111.cs
@@ -13,23 +13,22 @@ namespace App472.WebUI.App_Start
         public static void AddToContext(ref IList<Order> orders, ref IList<Product> prods, ref EFDBContext context, ref Int32 orderId)
         {
             Int32 userId = 111;
+            DateTimeOffset now = DateTimeOffset.Now;
+            DateTimeOffset yesterday = now.AddDays(-1);
+
+            Order order1 = new Order(orderId++, userId, null);
             Product prod1 = prods[0];
             Product prod2 = prods[1];
             Product prod3 = prods[2];
-            DateTimeOffset now = DateTimeOffset.Now;
-            DateTimeOffset yesterday = now.AddDays(-1);
             OrderedProduct op1 = new OrderedProduct();
             OrderedProduct op2 = new OrderedProduct();
             OrderedProduct op3 = new OrderedProduct();
             op1.Product = prod1; op1.Quantity = 1;
             op2.Product = prod2; op2.Quantity = 2;
             op3.Product = prod3; op3.Quantity = 1;
-
-            Order order1 = new Order(orderId++, userId, null);
             op1.Order = order1; order1.OrderedProducts.Add(op1);
             op2.Order = order1; order1.OrderedProducts.Add(op2);
             op3.Order = order1; order1.OrderedProducts.Add(op3);
-
             order1.OrderPlacedDate = yesterday;
             order1.PaymentReceivedDate = yesterday;
             order1.ReadyToShipDate = now;
@@ -39,10 +38,32 @@ namespace App472.WebUI.App_Start
             order1.ShippingAddress = "20 Enterprise Ave, Two Rocks, Perth Western Australia";
             order1.OrderStatus = "ReadyToShip";
             orders.Add(order1);
-
-            context.Orders.AddRange(orders);
             context.Entry(prod1).State = EntityState.Added;
             context.Entry(prod2).State = EntityState.Added;
+            context.Entry(prod3).State = EntityState.Added;
+
+            Order order2 = new Order(orderId++, userId, null);
+            Product prod4 = prods[3];
+            Product prod5 = prods[4];
+            OrderedProduct op4 = new OrderedProduct();
+            OrderedProduct op5 = new OrderedProduct();
+            op4.Product = prod4; op4.Quantity = 2;
+            op5.Product = prod5; op5.Quantity = 2;
+            op4.Order = order2; order2.OrderedProducts.Add(op4);
+            op5.Order = order2; order2.OrderedProducts.Add(op5);
+            order2.OrderPlacedDate = yesterday;
+            order2.PaymentReceivedDate = yesterday;
+            order2.ReadyToShipDate = now;
+            order2.ShipDate = null;
+            order2.ReceivedDate = null;
+            order2.BillingAddress = "20 Enterprise Ave, Two Rocks, Perth Western Australia";
+            order2.ShippingAddress = "20 Enterprise Ave, Two Rocks, Perth Western Australia";
+            order2.OrderStatus = "ReadyToShip";
+            orders.Add(order2);
+            context.Entry(prod4).State = EntityState.Added;
+            context.Entry(prod5).State = EntityState.Added;
+
+            context.Orders.AddRange(orders);
         }
     }
 }
--- App472.WebUI/App_Start/Seed/Orders112.cs
+++ App472.WebUI/App_Start/Seed/Orders112.cs
@@ -3,6 +3,7 @@ using App472.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Linq;
 using System.Web;
 
@@ -14,11 +15,21 @@ namespace App472.WebUI.App_Start
         {
             Int32 userId = 112;
             Product prod3 = prods[3];
+            DateTimeOffset now = DateTimeOffset.Now;
+            DateTimeOffset yesterday = now.AddDays(-1);
             OrderedProduct op3 = new OrderedProduct();
             op3.Product = prod3; op3.Quantity = 3;
 
             Order order3 = new Order(orderId++, userId, null);
             op3.Order = order3; order3.OrderedProducts.Add(op3);
+            order3.OrderPlacedDate = yesterday;
+            order3.PaymentReceivedDate = yesterday;
+            order3.ReadyToShipDate = now;
+            order3.ShipDate = null;
+            order3.ReceivedDate = null;
+            order3.BillingAddress = "20 Enterprise Ave, Two Rocks, Perth Western Australia";
+            order3.ShippingAddress = "20 Enterprise Ave, Two Rocks, Perth Western Australia";
+            order3.OrderStatus = "OrderPlaced";
             orders.Add(order3);
 
             context.Orders.AddRange(orders);
