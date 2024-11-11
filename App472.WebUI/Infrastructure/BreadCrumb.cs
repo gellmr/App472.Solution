30f686b6a2462e90fd57557178eed75c8d7763eb
gellmr <gellmr@users.noreply.github.com>
2024-11-11 11:58:52 +0800

    Added BreadCrumb class and partial

---
+++ App472.WebUI/Infrastructure/BreadCrumb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App472.WebUI.Infrastructure
+{
+    public class BreadCrumb
+    {
+        public BreadCrumb Child{ get; set; }
+        public string URL { get; set;}
+        public string LinkText{ get; set;}
+    }
+}
