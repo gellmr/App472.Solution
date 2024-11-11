421b5170e9eeee5a1340b0e37d0fb9fcc7697955
gellmr <gellmr@users.noreply.github.com>
2024-11-11 18:37:17 +0800

    Added _TableRowFullUser, _TableRowGuest and AdminUserAccIndex.js

---
+++ App472.WebUI/Scripts/AdminUserAccIndex.js
@@ -0,0 +1,31 @@
+﻿(function ($) {
+    // This file uses the Module Pattern
+    // https://alistapart.com/article/the-design-of-code-organizing-javascript/
+    //
+    $.adminUserAccIndex = function (options) {
+        var page = {
+            // Merge the passed options, into our internal options
+            options: $.extend({
+                //'duration': 500
+            }, options),
+            // --------------------------------------
+
+            // member variables
+            EnableListeners: function (){},
+            DisableListeners: function () { },
+
+            // --------------------------------------
+            // Page ready, attach event listeners
+            ReadyJs: function () {
+                page.EnableListeners();
+            },
+        };
+        return {
+            ready: page.ReadyJs
+        };
+    };
+})(jQuery);
+var options = {
+};
+var page = $.adminUserAccIndex(options);
+jQuery(document).ready(page.ready);
