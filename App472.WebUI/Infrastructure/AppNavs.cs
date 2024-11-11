a18396b9382743b5be5ddf1a2ff3c3b50b23ab8c
gellmr <gellmr@users.noreply.github.com>
2024-11-11 12:00:13 +0800

    Added AppNavs class

---
+++ App472.WebUI/Infrastructure/AppNavs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App472.WebUI.Infrastructure
+{
+    public static class AppNavs
+    {
+        // Text appearing on nav buttons
+        public static string ProductsNavText = "Edit Products";
+        public static string OrdersNavText = "Orders Backlog";
+        public static string UsersNavText = "User Accounts";
+
+        // Routes for actions. Dont end them with a slash /
+        public static string AdminOrders_Index = "/AdminOrders/Index";
+        public static string AdminUserAcc_Index = "/AdminUserAcc/Index";
+        public static string AdminUserOrder_Index = "/AdminUserOrder/Index";
+    }
+}
