73c1f30e8ed0f4a21575a01a1f615cf09ebe9121
gellmr <gellmr@users.noreply.github.com>
2024-11-11 12:01:57 +0800

    Added AdminOrderDetailViewModel.cs

---
+++ App472.WebUI/Models/AdminOrderDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using App472.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using App472.WebUI.Infrastructure;
+
+namespace App472.WebUI.Models
+{
+    public class AdminOrderDetailViewModel: AdminBaseOrderDetailViewModel
+    {
+        public Nullable<Int32> UserId { get; set; }
+        public Nullable<Guid> GuestId { get; set; }
+        public string UserName { get; set; }
+        public string OrderName { get; set; }
+        public BreadCrumb BCNavTrail { get; set; }
+    }
+}
